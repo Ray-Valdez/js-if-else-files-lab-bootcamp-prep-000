c7e8a64cd3ca207b31c05fedd45cb656413b9d0b
ray_valdez@hotmail.co.uk <ray_valdez@hotmail.co.uk>
2018-08-22 15:06:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var name = "Joe"
 var height = "74"
 
-if (name === "Joe") {tests.js
+if (name === "Joe") {console.log("Susan")
+
+if (height==="74") {console.log("70")
+
 // Don't worry about this
 module.exports = { name, height
 }
